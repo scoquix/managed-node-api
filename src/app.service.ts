a322b9aa9eee8036a9257ee2bfedacd1ab5d5053
scoquix <sebaszafranski@gmail.com>
2022-01-16 19:19:47 +0100

    Added service file

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome in my node rest api!';
+    return 'Welcome Andzia!';
   }
 }
